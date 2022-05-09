bb11598bb3ab295240d04b54ee438f2dde55714a
ShSato4 <shsato4@ShSato4noMacBook-Air.local>
2022-05-09 23:54:44 +0900

    ちょっと近づいた

--- src/sudoku-utils.ts
+++ src/sudoku-utils.ts
@@ -5,6 +5,22 @@ export const isValidDoubleCheck = <T>(ary: Array<T>) => {
   return 0 < t.length ? true : false
 }
 
+export const convertSudokuFormat = (ary: number[][][][]) => {
+  const hMatrix = []
+  for (let h = 0; h < 3; h++) {
+    const vMatrix = []
+    for (let v = 0; v < 3; v++) {
+      const yMatrix = []
+      for (let y = 0; y < 3; y++) {
+        yMatrix.push([[ary[h][v][y][0]], [ary[h][v][y][1]], [ary[h][v][y][2]]])
+      }
+      vMatrix.push(yMatrix)
+    }
+    hMatrix.push(vMatrix)
+  }
+  return hMatrix
+}
+
 export const initMatrix = () => {
   const hMatrix = []
   for (let h = 0; h < 3; h++) {
--- src/sudoku.ts
+++ src/sudoku.ts
@@ -1,13 +1,17 @@
-import { data as fields, values } from './sudoku-data.js'
+import { data, values } from './sudoku-data.js'
 import { VERTICAL, VerticalType, HORIZONTAL, HorizontalType } from './sudoku-types.js'
 import * as Utils from './sudoku-utils.js'
+
+const fields = Utils.convertSudokuFormat(data)
 
 // 縦軸同値チェック
 const isExistsSameValueOnVertical = (fieldX: HorizontalType, cellX: number, checkVal: number) => {
   let chkList: Array<number> = [checkVal]
   for (const v of Object.values(VERTICAL)) {
     for (let i = 0; i < 3; i++) {
-      if (checkVal === fields[v][fieldX][i][cellX]) return true
+      if (fields[v][fieldX][i][cellX].length === 1 && checkVal === fields[v][fieldX][i][cellX][0]) {
+        return true
+      }
     }
   }
   return false
@@ -20,7 +24,9 @@ const isExistsSameValueOnHorizontal = (fieldY: VerticalType, cellY: number, chec
   let chkList: Array<number> = [checkVal]
   for (const v of Object.values(HORIZONTAL)) {
     for (let i = 0; i < 3; i++) {
-      if (checkVal === fields[fieldY][v][cellY][i]) return true
+      if (fields[fieldY][v][cellY][i].length === 1 && checkVal === fields[fieldY][v][cellY][i][0]) {
+        return true
+      }
     }
   }
   return false
@@ -36,7 +42,12 @@ const isExistsSameValueInField = (
 ) => {
   for (let y = 0; y < 3; y++) {
     for (let x = 0; x < 3; x++) {
-      if (checkVal === fields[fieldY][fieldX][y][x]) return true
+      if (
+        fields[fieldY][fieldX][y][x].length === 1 &&
+        checkVal === fields[fieldY][fieldX][y][x][0]
+      ) {
+        return true
+      }
     }
   }
   return false
@@ -48,50 +59,76 @@ const getFieldCandidatesList = (fieldY: VerticalType, fieldX: HorizontalType) =>
   let tmpAry: number[] = Array()
   for (let y = 0; y < 3; y++) {
     for (let x = 0; x < 3; x++) {
-      if (fields[fieldY][fieldX][y][x] !== 0) {
-        tmpAry.push(fields[fieldY][fieldX][y][x])
+      if (fields[fieldY][fieldX][y][x].length === 0 && fields[fieldY][fieldX][y][x][0] !== 0) {
+        tmpAry.push(fields[fieldY][fieldX][y][x][0])
       }
     }
   }
   return values.filter((v) => !tmpAry.includes(v))
 }
 
-// console.log(getFieldCandidatesList(VERTICAL.TOP, HORIZONTAL.LEFT))
-
-const saveAry = Utils.initMatrix()
-for (const v of Object.values(VERTICAL)) {
-  for (const h of Object.values(HORIZONTAL)) {
-    // 候補リストを取得
-    const candidatesList = getFieldCandidatesList(v, h)
-    for (let y = 0; y < 3; y++) {
-      for (let x = 0; x < 3; x++) {
-        const insertList: number[] = []
-        if (fields[v][h][y][x] === 0) {
-          // 候補リストの中からさらに絞り込む
-          const tempAry: number[] = []
-          for (const val of candidatesList) {
-            if (
-              !isExistsSameValueOnVertical(h, x, Number(val)) &&
-              !isExistsSameValueOnHorizontal(v, y, Number(val))
-            ) {
-              tempAry.push(Number(val))
-            }
-          }
-          saveAry[v][h][y][x] = tempAry
-        } else {
-          saveAry[v][h][y][x] = [fields[v][h][y][x]]
+const isExistsCandidatesData = () => {
+  for (const v of Object.values(VERTICAL)) {
+    for (const h of Object.values(HORIZONTAL)) {
+      for (let y = 0; y < 3; y++) {
+        for (let x = 0; x < 3; x++) {
+          if (1 < fields[v][h][y][x].length) return true
         }
-        //candidatesList[v][h][y].push(insertList)
       }
     }
   }
+  return false
 }
 
-const list = getFieldCandidatesList(VERTICAL.TOP, HORIZONTAL.LEFT)
-const temp = []
-for (let y = 0; y < 3; y++) {
-  for (let x = 0; x < 3; x++) {
-    if (fields[VERTICAL.TOP][HORIZONTAL.LEFT][y][x] === 0) {
+// console.log(getFieldCandidatesList(VERTICAL.TOP, HORIZONTAL.LEFT))
+
+//const fields = Utils.initMatrix()
+let count = 1
+while (isExistsCandidatesData) {
+  for (const v of Object.values(VERTICAL)) {
+    for (const h of Object.values(HORIZONTAL)) {
+      // 候補リストを取得
+      const candidatesList = getFieldCandidatesList(v, h)
+      for (let y = 0; y < 3; y++) {
+        for (let x = 0; x < 3; x++) {
+          //const insertList: number[] = []
+          if (fields[v][h][y][x].length === 1) {
+            if (fields[v][h][y][x][0] === 0) {
+              // 候補リストの中からさらに絞り込む
+              const tempAry: number[] = []
+              for (const val of candidatesList) {
+                if (
+                  !isExistsSameValueOnVertical(h, x, Number(val)) &&
+                  !isExistsSameValueOnHorizontal(v, y, Number(val)) &&
+                  !isExistsSameValueInField(v, h, Number(val))
+                ) {
+                  tempAry.push(Number(val))
+                }
+              }
+              fields[v][h][y][x] = tempAry
+            } else {
+              fields[v][h][y][x] = [fields[v][h][y][x][0]]
+            }
+          } else {
+            const tempAry: number[] = []
+            for (const val of fields[v][h][y][x]) {
+              if (
+                !isExistsSameValueOnVertical(h, x, Number(val)) &&
+                !isExistsSameValueOnHorizontal(v, y, Number(val)) &&
+                !isExistsSameValueInField(v, h, Number(val))
+              ) {
+                tempAry.push(Number(val))
+              }
+            }
+            fields[v][h][y][x] = tempAry
+          }
+          //candidatesList[v][h][y].push(insertList)
+        }
+      }
     }
   }
+  console.log(`count: ${count}`)
+  count++
 }
+
+console.log('fin')
